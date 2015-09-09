99a4fcafed1f795e0d640c9f5c15994745b55d4e
btasdoven <btasdoven@gmail.com>
2015-09-09 23:20:34 +0300

    python call is updated

--- read_poll.sh
+++ read_poll.sh
@@ -6,5 +6,5 @@ GROUP_NAME="Deneme"
 POLL_ID="$1"
 
 JSON_VOTES=$(phantomjs --ssl-protocol=any read_poll.js "$UNAME" "$PASS" "$GROUP_NAME" "$POLL_ID")
-python check_votes.py "$JSON_VOTES"
+python print_votes.py "$JSON_VOTES"
 
