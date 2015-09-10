b88802eb82cca891bf9ffff0b3a3f6e03ca63350
Batuhan Tasdoven <btasdoven@gmail.com>
2015-09-10 18:34:05 +0300

    print_votes is integrated with send_post

--- print_votes.py
+++ print_votes.py
@@ -1,9 +1,29 @@
 import sys, json
+import datetime
 
 votes = json.loads(sys.argv[1])
+uname = sys.argv[2]
+passwd = sys.argv[3]
 
-for date, vote in votes.iteritems():
-	print date.encode('utf-8'), vote
+it = votes.iteritems()
+it = sorted(it, key=lambda x: (x[0], x[1]))
+
+weekday = datetime.datetime.today().weekday()
+tomorrow_name, tomorrow_vote_count = it[weekday+1]
+
+if tomorrow_vote_count >= 6:
+	message = "Yarin(%s) %d kisi oynayalim demis. Oynayalim diyen herkes gelecek degil mi?" % (tomorrow_name, tomorrow_vote_count)
+else:
+	message = "Yarin(%s) %d kisi oynayalim dedigi icin yeterli sayiya ulasamadik." % (tomorrow_name, tomorrow_vote_count)
+
+message += "\n\n Su anlik bu haftanin oylama sonuclari soyle:\n\n"	
+
+for date, vote in it:
+	message += "%s, %s\n" % (date.encode('utf-8'), vote)
 	
+cmd = ["./send_post.sh", uname, passwd, message]
+process = Popen(cmd, stdout=PIPE)
+output, err = process.communicate()
+		
 
 
