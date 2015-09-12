81251e48583703a58b9cb6ca0d8a94a7faea413a
Batuhan Taşdöven <btasdoven@gmail.com>
2015-09-12 11:04:21 +0300

    Update read_poll.sh

--- read_poll.sh
+++ read_poll.sh
@@ -6,6 +6,14 @@ GROUP_NAME="$3"
 POLL_ID="$4"
 
 VOTES=$(phantomjs --ssl-protocol=any js/read_poll.js "$UNAME" "$PASS" "$GROUP_NAME" "$POLL_ID")
+MEMBERS=$(phantomjs --ssl-protocol=any js/get_members.js  "$UNAME" "$PASS" "$GROUP_NAME")
+
+MESSAGE=$(python generate_message.py "$VOTES" "$MEMBERS")
+if [[ ! -z $MESSAGE ]]; then
+	./send_post.sh $UNAME $PASS $GROUP_NAME $MESSAGE
+else
+	echo "No need to send a post. Aborting"
+fi
 #python print_votes.py "$JSON_VOTES" "$1" "$2"
 
 echo $VOTES
